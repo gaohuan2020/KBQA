7c994f0c9d30503dddce860dda5fd34ec90ccb51
Huan Gao <huaga@microsoft.com>
2020-10-09 10:35:28 +0800

    Add tqdm

--- NLPUnitTest/LSTMCRFModel_test.py
+++ NLPUnitTest/LSTMCRFModel_test.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 from NLP.Helper.NLPHelper import *
 from NLP.Model.EntityRelationReorganization.Model import LSTMCRF
 from NLP.Config.Setting import *
+from tqdm import trange
 
 class LSTMCRFModel_test(unittest.TestCase):
     def test_lstmCRFModel(self):
@@ -28,25 +29,15 @@ class LSTMCRFModel_test(unittest.TestCase):
         model = LSTMCRF(len(word_to_ix), tag_to_ix)
         optimizer = optim.SGD(model.parameters(), lr=0.01, weight_decay=1e-4)
 
-        # Check predictions before training
         with torch.no_grad():
             precheck_sent = prepare_sequence(training_data[0][0], word_to_ix)
             precheck_tags = torch.tensor([tag_to_ix[t] for t in training_data[0][1]], dtype=torch.long)
             print(model(precheck_sent))
 
-        # Make sure prepare_sequence from earlier in the LSTM section is loaded
-        for epoch in range(100):  # again, normally you would NOT do 300 epochs, it is toy data
-            # Step 1. Remember that Pytorch accumulates gradients.
-            # We need to clear them out before each instance
+        for i in trange(100):
             model.zero_grad()
-            # Step 2. Get our batch inputs ready for the network, that is,
-            # turn them into Tensors of word indices.
-            # If training_data can't be included in one batch, you need to sample them to build a batch
             sentence_in_pad, targets_pad = prepare_sequence_batch(training_data, word_to_ix, tag_to_ix)
-            # Step 3. Run our forward pass.
             loss = model.neg_log_likelihood_parallel(sentence_in_pad, targets_pad)
-            # Step 4. Compute the loss, gradients, and update the parameters by
-            # calling optimizer.step()
             loss.backward()
             print(loss.item())
             optimizer.step()
